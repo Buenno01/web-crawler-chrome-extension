20ff9c73ba840059a00c4d0ef44122da2647df45
Vinícius Bueno Costa <buenov535@gmail.com>
2025-06-16 23:19:06 -0300

    feat: added storage cleared visual feedback

--- src/pages/Reports.jsx
+++ src/pages/Reports.jsx
@@ -5,13 +5,13 @@ import MessageBox from '../components/ui/MessageBox';
 
 function Reports() {
   const { t } = useTranslation();
-  const { data } = useDataExtractionContext();
+  const { extractedData } = useDataExtractionContext();
 
   return (
     <>
       <h1 className='text-2xl font-bold mb-4'>{t('reportsTitle')}</h1>
       {
-        !data && (
+        !extractedData && (
           <MessageBox
             title={t('noDataTitle')}
             variant='info'
--- src/pages/Storage.jsx
+++ src/pages/Storage.jsx
@@ -1,23 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useTranslation } from '../hooks/useTranslation';
 import Button from '../components/ui/Button';
 import { useDataExtractionContext } from '../contexts/dataExtractionContext';
-import { AiOutlineClear } from "react-icons/ai";
+import { AiOutlineClear, AiOutlineCheckCircle } from "react-icons/ai";
+
 
 function Storage() {
   const { t } = useTranslation();
   const { clearData } = useDataExtractionContext();
+  const [cleared, setCleared] = useState(false);
+
+  const handleClearStorage = () => {
+    clearData();
+    setCleared(true);
+  }
 
   return (
     <>
       <h1 className='text-2xl font-bold mb-4'>{t('storageTitle')}</h1>
       <div className='box'>
         <Button
-          variant='danger'
-          onClick={clearData}
+          variant={ cleared ? 'success' : 'danger'}
+          onClick={handleClearStorage}
         >
-          <AiOutlineClear className='text-lg' />
-          {t('storageClear')}
+          {
+            cleared ? (
+              <>
+                <AiOutlineCheckCircle className='text-lg' />
+                {t('storageCleared')}
+              </>
+            ) : (
+              <>
+                <AiOutlineClear className='text-lg' />
+                {t('storageClear')}
+              </>
+            )
+          }
         </Button>
       </div>
     </>
