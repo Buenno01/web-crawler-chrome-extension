345c5827e05b85a61004ab68573f77556cf4a9e6
Vinícius Bueno Costa <buenov535@gmail.com>
2025-06-16 23:36:14 -0300

    feat: added loading feedback for used storage

--- src/pages/Storage.jsx
+++ src/pages/Storage.jsx
@@ -10,29 +10,50 @@ function Storage() {
   const { t } = useTranslation();
   const { clearData } = useDataExtractionContext();
   const [cleared, setCleared] = useState(false);
-  const [usedStorage, setUsedStorage] = useState(0);
   const [formattedUsedStorage, setFormattedUsedStorage] = useState('0 bytes');
+  const [loading, setLoading] = useState(true);
 
   const handleClearStorage = () => {
     clearData();
     setCleared(true);
+    getUsedStorage();
   }
 
   useEffect(() => {
-    const getUsedStorage = async () => {
-      const bytes = await chrome.storage.local.getBytesInUse(null);
-      setUsedStorage(bytes);
-      setFormattedUsedStorage(formatBytes(bytes));
-    }
     getUsedStorage();
   }, []);
+
+  const getUsedStorage = async () => {
+    /* If it is too quick, wait 0.5 seconds */
+    try {
+      const timer = Date.now();
+      setLoading(true);
+      const bytes = await chrome.storage.local.getBytesInUse(null);
+      const timeElapsed = Date.now() - timer;
+      if (timeElapsed < 500) {
+        await new Promise(resolve => setTimeout(resolve, 500 - timeElapsed));
+      }
+      setFormattedUsedStorage(formatBytes(bytes));
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
 
   return (
     <>
       <h1 className='text-2xl font-bold mb-4'>{t('storageTitle')}</h1>
       <div className='box'>
         <div className='flex flex-col gap-2'>
-          <p>{t('storageUsed')}: {formattedUsedStorage}</p>
+          {
+            loading ? (
+              <p>{t('storageLoading')}</p>
+            ) : (
+              <p>{t('storageUsed')}: {formattedUsedStorage}</p>
+            )
+          }
         </div>
         <Button
           variant={ cleared ? 'success' : 'danger'}
