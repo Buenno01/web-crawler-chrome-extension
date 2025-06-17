a3354b5ff323223ba92c73ffb5c8aa1b1954b428
Vinícius Bueno Costa <buenov535@gmail.com>
2025-06-16 23:28:43 -0300

    feat: added used storage information

--- src/pages/Storage.jsx
+++ src/pages/Storage.jsx
@@ -1,24 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useTranslation } from '../hooks/useTranslation';
 import Button from '../components/ui/Button';
 import { useDataExtractionContext } from '../contexts/dataExtractionContext';
 import { AiOutlineClear, AiOutlineCheckCircle } from "react-icons/ai";
+import { formatBytes } from '../utils/formatBytes';
 
 
 function Storage() {
   const { t } = useTranslation();
   const { clearData } = useDataExtractionContext();
   const [cleared, setCleared] = useState(false);
+  const [usedStorage, setUsedStorage] = useState(0);
+  const [formattedUsedStorage, setFormattedUsedStorage] = useState('0 bytes');
 
   const handleClearStorage = () => {
     clearData();
     setCleared(true);
   }
 
+  useEffect(() => {
+    const getUsedStorage = async () => {
+      const bytes = await chrome.storage.local.getBytesInUse(null);
+      setUsedStorage(bytes);
+      setFormattedUsedStorage(formatBytes(bytes));
+    }
+    getUsedStorage();
+  }, []);
+
   return (
     <>
       <h1 className='text-2xl font-bold mb-4'>{t('storageTitle')}</h1>
       <div className='box'>
+        <div className='flex flex-col gap-2'>
+          <p>{t('storageUsed')}: {formattedUsedStorage}</p>
+        </div>
         <Button
           variant={ cleared ? 'success' : 'danger'}
           onClick={handleClearStorage}
---
+++ src/utils/formatBytes.js
@@ -0,0 +1,10 @@
+export const formatBytes = (bytes, decimals = 2) => {
+  if (!bytes || bytes === 0) return '0 Bytes';
+
+  const k = 1024;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return `${parseFloat((bytes / Math.pow(k, i)).toFixed(decimals))} ${sizes[i]}`;
+}; 
