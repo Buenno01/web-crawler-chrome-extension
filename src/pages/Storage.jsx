837f32ee6a5d9938dd8f36b705f30ce1142ef104
Vinícius Bueno Costa <buenov535@gmail.com>
2025-06-16 23:08:00 -0300

    feat: added clear storage button

--- src/pages/Storage.jsx
+++ src/pages/Storage.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import { useTranslation } from '../hooks/useTranslation';
+import Button from '../components/ui/Button';
+import { useDataExtractionContext } from '../contexts/dataExtractionContext';
+import { AiOutlineClear } from "react-icons/ai";
 
 function Storage() {
   const { t } = useTranslation();
+  const { clearData } = useDataExtractionContext();
 
   return (
     <>
       <h1 className='text-2xl font-bold mb-4'>{t('storageTitle')}</h1>
+      <div className='box'>
+        <Button
+          variant='danger'
+          onClick={clearData}
+        >
+          <AiOutlineClear className='text-lg' />
+          {t('storageClear')}
+        </Button>
+      </div>
     </>
   )
 }
