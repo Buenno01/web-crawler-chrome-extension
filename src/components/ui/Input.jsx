b7a9f81e75e3196b6a57f902a5cd05f115778726
Vinicius Bueno <buenov535@gmail.com>
2026-01-07 15:42:03 -0300

    refacotr: removed unused imports

--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 function Input({ label, className = '', ...props }) {
   return (
     <div className="form__group">
--- src/pages/Storage.jsx
+++ src/pages/Storage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { useTranslation } from '../hooks/useTranslation';
 import Button from '../components/ui/Button';
 import { useDataExtractionContext } from '../contexts/dataExtractionContext';
